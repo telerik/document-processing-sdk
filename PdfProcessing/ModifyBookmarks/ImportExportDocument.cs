c9bcd56927635ee6d24985b5e315db835d542845
Tanya Dimitrova <tdimitrova@progress.com>
2019-10-31 16:32:16 +0200

    Update examples

---
+++ PdfProcessing/ModifyBookmarks/ImportExportDocument.cs
@@ -0,0 +1,31 @@
+﻿using System.Diagnostics;
+using System.IO;
+using Telerik.Windows.Documents.Fixed.FormatProviders.Pdf;
+using Telerik.Windows.Documents.Fixed.Model;
+
+namespace ModifyBookmarks
+{
+    public static class ImportExportDocument
+    {
+        public static readonly string InputFile ="Resources/SampleDocument.pdf";
+        private static PdfFormatProvider provider = new PdfFormatProvider();
+
+        public static RadFixedDocument ImportDocument()
+        {
+            return provider.Import(File.ReadAllBytes(InputFile));
+        }
+
+        public static void ExportDocument(RadFixedDocument document)
+        {
+            string modifiedFileName = "ModifiedDocument.pdf";
+
+            if (File.Exists(modifiedFileName))
+            {
+                File.Delete(modifiedFileName);
+            }
+
+            File.WriteAllBytes(modifiedFileName, provider.Export(document));
+            Process.Start(modifiedFileName);
+        }
+    }
+}
