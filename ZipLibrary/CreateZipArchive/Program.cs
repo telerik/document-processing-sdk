e0c8fc6ecdce3e93c0a2f3040d8a6244bcf72d82
PROGRESS\ykaraman <ykaraman@progress.com>
2024-03-20 13:33:11 +0200

    Reversed changes in existing example and updated it with new static method.

--- ZipLibrary/CreateZipArchive/Program.cs
+++ ZipLibrary/CreateZipArchive/Program.cs
@@ -16,18 +16,7 @@ namespace CreateZipArchive_NetStandard
 
             using (Stream stream = File.Open(zipFileName, FileMode.Create))
             {
-
-                //By default the EncryptionStrenght is 256 bits but it can be explicitly specified (EncryptionStrength.Aes128, EncryptionStrength.Aes192, and EncryptionStrength.Aes256) by passing it to the constructor 
-                PasswordEncryptionSettings aesEncryptionSettings = EncryptionSettings.CreateAesPasswordEncryptionSettings(); 
- 
-                //You can also use the PKWARE encryption algorithm instead of the AES one 
-                PasswordEncryptionSettings pkwareEncryptionSettings = EncryptionSettings.CreatePkzipPasswordEncryptionSettings(); 
- 
-                aesEncryptionSettings.Password = "password";  
-                CompressionSettings compressionSettings = null; 
-                Encoding encoding = null; 
-
-                using (ZipArchive archive = ZipArchive.Create(stream, encoding, compressionSettings, aesEncryptionSettings))
+                using (ZipArchive archive = ZipArchive.Create(stream, entryNameEncoding: null))
                 {
                     using (ZipArchiveEntry entry = archive.CreateEntry("text.txt"))
                     {
