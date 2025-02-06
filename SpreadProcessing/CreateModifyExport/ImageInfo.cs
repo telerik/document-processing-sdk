4b0067a08f8eddf5371d2b0b4a9c29eafe79c506
PROGRESS\velcheva <velcheva@progress.com>
2025-02-06 16:14:09 +0200

    Replaced bitmap with skia

--- SpreadProcessing/CreateModifyExport/ImageInfo.cs
+++ SpreadProcessing/CreateModifyExport/ImageInfo.cs
@@ -10,10 +10,9 @@ namespace CreateModifyExport
         public override Size GetImageSize(byte[] imageData)
         {
             MemoryStream stream = new MemoryStream(imageData);
-            using (System.Drawing.Bitmap image = new System.Drawing.Bitmap(stream))
-            {
-                return new Size(image.Width, image.Height);
-            }
+
+            SkiaSharp.SKBitmap image = SkiaSharp.SKBitmap.Decode(stream);
+            return new Size(image.Width, image.Height);
         }
 
         public override bool TryGetRawImageData(byte[] imageData, out byte[] rawRgbData, out byte[] rawAlpha, out Size size)
