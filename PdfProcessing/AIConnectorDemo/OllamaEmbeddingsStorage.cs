afc7c017b10ca88e5b76d8128d6ddcf0a5754a45
PROGRESS\velcheva <velcheva@progress.com>
2025-07-03 15:19:20 +0300

    AI Connector demo

---
+++ PdfProcessing/AIConnectorDemo/OllamaEmbeddingsStorage.cs
@@ -0,0 +1,67 @@
+﻿using LangChain.Databases;
+using LangChain.Databases.Sqlite;
+using LangChain.DocumentLoaders;
+using LangChain.Extensions;
+using LangChain.Providers;
+using LangChain.Providers.Ollama;
+using Telerik.Documents.AIConnector;
+
+namespace AIConnectorDemo
+{
+    // Necessary steps to get this working:
+    // 1. Install Ollama: https://ollama.com/
+    // 2. Pull the all-minilm model we'll use for embeddings: ollama pull all-minilm
+    // 3. Ensure Ollama is running: ollama serve
+    internal class OllamaEmbeddingsStorage : IEmbeddingsStorage
+    {
+        private const string AllMinilmEmbeddingModelName = "all-minilm";
+        private const string DBName = "vectors.db";
+        private const int DimensionsForAllMinilm = 384; // Should be 384 for all-minilm
+        private static readonly string defaultCollectionName = "defaultName";
+
+        private readonly SqLiteVectorDatabase vectorDatabase;
+        private readonly OllamaEmbeddingModel embeddingModel;
+
+        IVectorCollection vectorCollection;
+
+        public OllamaEmbeddingsStorage()
+        {
+            OllamaProvider provider = new OllamaProvider();
+            this.embeddingModel = new OllamaEmbeddingModel(provider, id: AllMinilmEmbeddingModelName);
+            this.vectorDatabase = new SqLiteVectorDatabase(dataSource: DBName);
+        }
+
+        public async Task<string> GetQuestionContext(string question)
+        {
+            IReadOnlyCollection<Document> similarDocuments = await this.vectorCollection.GetSimilarDocuments(this.embeddingModel, question, amount: 5);
+
+            return similarDocuments.AsString();
+        }
+
+        public void SetText(string text, PartialContextProcessorSettings settings)
+        {
+            MemoryStream memoryStream = new MemoryStream();
+            StreamWriter writer = new StreamWriter(memoryStream);
+            writer.Write(text);
+
+            if (this.vectorDatabase.IsCollectionExistsAsync(defaultCollectionName).Result)
+            {
+                this.vectorDatabase.DeleteCollectionAsync(defaultCollectionName).Wait();
+            }
+
+            this.vectorCollection = this.vectorDatabase.AddDocumentsFromAsync<TextLoader>(
+                this.embeddingModel,
+                dimensions: DimensionsForAllMinilm,
+                dataSource: DataSource.FromBytes(memoryStream.ToArray()),
+                textSplitter: null,
+                collectionName: defaultCollectionName,
+                behavior: AddDocumentsToDatabaseBehavior.JustReturnCollectionIfCollectionIsAlreadyExists).Result;
+
+        }
+
+        public void Dispose()
+        {
+            this.vectorDatabase.Dispose();
+        }
+    }
+}
---
+++ PdfProcessing/AIConnectorDemo/Program.cs
@@ -0,0 +1,100 @@
+﻿using Azure.AI.OpenAI;
+using Microsoft.Extensions.AI;
+using OpenAI.Chat;
+using System.IO;
+#if NETWINDOWS
+using Telerik.Windows.Documents.AIConnector;
+#else
+using Telerik.Documents.AIConnector;
+#endif
+using Telerik.Windows.Documents.Fixed.FormatProviders.Pdf;
+using Telerik.Windows.Documents.Fixed.Model;
+using Telerik.Windows.Documents.TextRepresentation;
+
+namespace AIConnectorDemo
+{
+    internal class Program
+    {
+        static int maxTokenCount = 128000;
+        static IChatClient iChatClient;
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+            CreateChatClient();
+
+            using (Stream input = File.OpenRead("John Grisham.pdf"))
+            {
+                PdfFormatProvider pdfFormatProvider = new PdfFormatProvider();
+                RadFixedDocument inputPdf = pdfFormatProvider.Import(input, null);
+                ISimpleTextDocument simpleDocument = inputPdf.ToSimpleTextDocument();
+
+                Summarize(simpleDocument);
+
+                Console.WriteLine("--------------------------------------------------");
+
+                AskQuestion(simpleDocument);
+
+                Console.WriteLine("--------------------------------------------------");
+
+                AskPartialContextQuestion(simpleDocument);
+            }
+        }
+
+        private static void CreateChatClient()
+        {
+            string key = Environment.GetEnvironmentVariable("AZUREOPENAI_KEY");
+            string endpoint = Environment.GetEnvironmentVariable("AZUREOPENAI_ENDPOINT");
+            string model = "gpt-4o-mini";
+
+            AzureOpenAIClient azureClient = new(
+                new Uri(endpoint),
+                new Azure.AzureKeyCredential(key),
+                new AzureOpenAIClientOptions());
+            ChatClient chatClient = azureClient.GetChatClient(model);
+
+            iChatClient = new OpenAIChatClient(chatClient);
+        }
+
+        private static void Summarize(ISimpleTextDocument simpleDocument)
+        {
+            SummarizationProcessor summarizationProcessor = new SummarizationProcessor(iChatClient, maxTokenCount);
+            summarizationProcessor.Settings.PromptAddition = "Summarize the text in a few sentences. Be concise and clear.";
+            summarizationProcessor.SummaryResourcesCalculated += SummarizationProcessor_SummaryResourcesCalculated;
+
+            string summary = summarizationProcessor.Summarize(simpleDocument).Result;
+            Console.WriteLine(summary);
+        }
+
+        private static void SummarizationProcessor_SummaryResourcesCalculated(object? sender, SummaryResourcesCalculatedEventArgs e)
+        {
+            Console.WriteLine($"The summary will require {e.EstimatedCallsRequired} calls and {e.EstimatedTokensRequired} tokens");
+            e.ShouldContinueExecution = true;
+        }
+
+        private static void AskQuestion(ISimpleTextDocument simpleDocument)
+        {
+            CompleteContextQuestionProcessor completeContextQuestionProcessor = new CompleteContextQuestionProcessor(iChatClient, maxTokenCount);
+
+            string question = "How many pages is the document and what is it about?";
+            string answer = completeContextQuestionProcessor.AnswerQuestion(simpleDocument, question).Result;
+            Console.WriteLine(question);
+            Console.WriteLine(answer);
+        }
+
+        private static void AskPartialContextQuestion(ISimpleTextDocument simpleDocument)
+        {
+#if NETWINDOWS
+            PartialContextQuestionProcessor partialContextQuestionProcessor = new PartialContextQuestionProcessor(iChatClient, maxTokenCount, simpleDocument);
+#else
+            IEmbeddingsStorage embeddingsStorage = new OllamaEmbeddingsStorage();
+            PartialContextQuestionProcessor partialContextQuestionProcessor = new PartialContextQuestionProcessor(iChatClient, embeddingsStorage, maxTokenCount, simpleDocument);
+#endif
+            string question = "What is the last book by John Grisham?";
+            string answer = partialContextQuestionProcessor.AnswerQuestion(question).Result;
+            Console.WriteLine(question);
+            Console.WriteLine(answer);
+        }
+    }
+}
