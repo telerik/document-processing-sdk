3bdeae3212e316cb022ba017538a008928d113b8
Desislava Yordanova <dyordano@progress.com>
2026-04-15 09:22:15 +0300

    Add SDK examples for PdfProcessing agent tools

---
+++ AITools/AgentToolsInPdfViewerWPF/App.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Windows;
+
+namespace AgentToolsInPdfViewerWPF
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        public App()
+        {
+           this.InitializeComponent();
+        }
+    }
+}
