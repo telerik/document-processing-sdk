c3aa174b86a8b3486afa7dfe66796810f7e90bf4
Desislava Yordanova <dyordano@progress.com>
2026-05-04 17:46:45 +0300

    feat: add SavingDataFromDatagridviewToXlsx SDK example
    Auto-generated from KB article: saving-data-from-datagridview-to-xlsx-file-in-csharp

---
+++ SpreadProcessing/SavingDataFromDatagridviewToXlsx/Program.cs
@@ -0,0 +1,19 @@
+// Auto-generated from: Exporting Data from DataGridView Control to Excel File
+// Source: saving-data-from-datagridview-to-xlsx-file-in-csharp
+
+using System;
+using System.Windows.Forms;
+
+namespace SavingDataFromDatagridviewToXlsx
+{
+    internal static class Program
+    {
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+}
---
+++ SpreadProcessing/SavingDataFromDatagridviewToXlsx/ReadMe.md
@@ -0,0 +1,4 @@
+## Exporting Data from DataGridView Control to Excel File
+This project demonstrates how to save data from a WinForms DataGridView control to an Excel XLSX file using RadSpreadProcessing. [Saving Data from DataGridView to XLSX File in C# Windows Forms](https://docs.telerik.com/devtools/document-processing/knowledge-base/saving-data-from-datagridview-to-xlsx-file-in-csharp)
+
+[//]: <keywords: radspreadprocessing, excel, xlsx, datagridview, winforms, export>
